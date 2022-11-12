7b198ccee72002a680ca15be1457179817476b42
reebass <store1993dron@gmail.com>
2022-11-12 15:09:43 +0200

    complete-first-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+// const startBtn = document.querySelector('[data-start]');
+// const stopBtn = document.querySelector('[data-stop]');
+// const body = document.querySelector('body');
+
+// startBtn.addEventListener('click', onStart);
+// stopBtn.addEventListener('click', onStop);
+
+// const idInt = setInterval((onStart) => {
+//     body.style.backgroundColor = getRandomHexColor()
+//     startBtn.disabled = true;} , 1000)
+
+// // function onStop() {
+
+// // }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', onStart)
+let idInterval = null;
+stopBtn.disabled = true;
+
+function onStart() {
+    body.style.backgroundColor = getRandomHexColor()
+    idInterval = setInterval(() => {body.style.backgroundColor = getRandomHexColor()}, 1000);
+    startBtn.toggleAttribute('disabled')
+    stopBtn.toggleAttribute('disabled')
+}
+
+stopBtn.addEventListener('click', onStop)
+
+function onStop() {
+    startBtn.toggleAttribute('disabled')
+    stopBtn.toggleAttribute('disabled')
+    clearInterval(idInterval)
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
 
